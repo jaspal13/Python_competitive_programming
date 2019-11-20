057a6abf1104ce48d626a50b446093da05d0e475
Jaspal Singh <jaspal.singh1303@gmail.com>
2019-11-19 21:06:54 -0500

    leet code problem 1115

---
+++ LeetCode/1115.py
@@ -0,0 +1,23 @@
+from threading import Lock
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.fooLock = Lock()
+        self.barLock = Lock()
+        self.barLock.acquire()
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.fooLock.acquire()
+            # printFoo() outputs "foo". Do not change or remove this line.
+            printFoo()
+            self.barLock.release()
+
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.barLock.acquire()
+            # printBar() outputs "bar". Do not change or remove this line.
+            printBar()
+            self.fooLock.release()
